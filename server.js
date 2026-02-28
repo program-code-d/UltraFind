a4cf7457477636493a39bbea022326ae267e8ba3
Alex Alex <alexalexg1024@gmail.com>
2026-02-28 00:06:11 -0600

    starting login

--- server.js
+++ server.js
@@ -2,39 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { measureMemory } = require('vm');
-
+const fs = require("fs");
+const crypto = require("crypto");
 app.use(express.json());
 app.use(express.static(__dirname, { index: false }));
 
-let messageArray=[];
+function hashPassword(passw_string)
+{
+  return crypto.createHash("sha256").update(String(passw_string)).digest("hex");
+}
+
 app.get('/', (req, res) =>
 {
     console.log('Here')
     res.sendFile(path.join(__dirname, 'login.html'));
 })
 
-app.post('/sendmessage', (req,res)=>
+app.post('/signup', (req,res)=>
 {
-   console.log(req.body);
-   messageArray=req.body.messages;
-   res.send('Message received');
-})
-app.get('/getupdate', (req,res)=>
-{
-    res.json(messageArray);
-})
-app.post('/password', (req,res)=>
-{
-    let password=req.body.password;
-    console.log(password)
-    if(password==1234)
-    {         
-//res.sendFile(path.join(__dirname, 'index.html'));
-          res.json({ok:1});
-    }
-    else
-    {
- res.json({ok:0});
-    }
+    
 })
 app.listen(8080)
