3a5f0ab84ae7e0f6c74fb6dc47dc358e4ea83609
isaac1-coder <betasoyourhomework@gmail.com>
2026-03-25 22:35:03 -0500

    Made search for listings work

--- server.js
+++ server.js
@@ -210,6 +210,24 @@ async function getListings(body) {
     }
 }
 
+async function searchListings(body) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        let saltResult = await conn.query("SELECT salt FROM Users WHERE email = ?", [body.email]);
+        const hashedPassword = hashPassword(body.password + saltResult[0].salt);
+        const loginQuery = "SELECT id FROM Users WHERE email = ? AND password = ?;";
+        const userRows = await conn.query(loginQuery, [body.email, hashedPassword]);
+        const insertQuery = "SELECT * FROM Listings WHERE title LIKE ? AND is_Active = true;";
+        const res = await conn.query(insertQuery, [`%${body.search}%`]);
+        return { success: true, listings: res, userExist: true };
+    } catch (err) {
+        return { success: false, userExist: false };
+    } finally {
+        if (conn) conn.release();
+    }
+}
+
 
 async function getMyListings(body) {
     let conn;
@@ -689,6 +707,21 @@ app.post('/loginauth', async (req, res) => {
 app.post('/getListings', async (req, res) => {
     try {
         const result = await getListings(req.body);
+        if (result && !result.userExist) {
+            return res.send({ message: "failed" });
+        }
+        if (result && result.success) {
+            res.send(result.listings);
+        }
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+});
+
+
+app.post('/searchListings', async (req, res) => {
+    try {
+        const result = await searchListings(req.body);
         if (result && !result.userExist) {
             return res.send({ message: "failed" });
         }
